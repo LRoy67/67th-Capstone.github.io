e16436c9867fc2187f732011d70dcdc86fee3093
LRoy67 <2503548@spsd.sk.ca>
2025-12-01 13:30:44 -0600

    Started on a character

---
+++ Capstone Main/sketch.js
@@ -0,0 +1,20 @@
+// Capstone Project
+// Logan Roy
+// December 1, 2025
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  wizard();
+}
+
+
+function wizard(){
+  // will create the wizard character
+  fill(220);
+  circle(width/2, height/2, 10);
+}
