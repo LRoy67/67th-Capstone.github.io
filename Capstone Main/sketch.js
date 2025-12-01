26f2ac6bc7f48e5096b41d9ea87b9b05e5b7b195
LRoy67 <2503548@spsd.sk.ca>
2025-12-01 13:59:26 -0600

    Update sketch.js

--- Capstone Main/sketch.js
+++ Capstone Main/sketch.js
@@ -15,6 +15,12 @@ function draw() {
 
 function wizard(){
   // will create the wizard character
+  fill(21, 101, 192);
+  rect(width/2 - 19.5, height/2 + 25, 40, 50)
+  
+  fill(255, 204, 153);
+  circle(width/2, height/2 + 5, 60);
+
   fill(220);
-  circle(width/2, height/2, 10);
+  arc(width/2, height/2, 60, 30, -1, 3);
 }
