0f33fd1df77cebc2673351b2d98d72c74f49c93e
LRoy67 <2503548@spsd.sk.ca>
2025-12-03 14:09:32 -0600

    made more characters and progessed already made ones

--- Capstone Main/sketch.js
+++ Capstone Main/sketch.js
@@ -11,11 +11,12 @@ function setup() {
 
 function draw() {
   background(0);
-  wizard();
-  staff();
+  //wizard();
+  //staff();
+  //assassin();
   knightguy.display();
   
-  //archer();
+  archer();
 }
 // class Wizard{
 // constructor(){
@@ -73,6 +74,64 @@ function staff(){
 
 // }
 
+function assassin(){
+  // will create the assassin
+  stroke(0)
+  strokeWeight(1)
+  fill(220, 20, 60);
+  rect(width/2 - 19.5, height/2 + 25, 40, 50)
+  
+  fill(0);
+  strokeWeight(0);
+  arc(width/2, height/2, 55, 60, 30, 20, CHORD, 2);
+  arc(width/2, height/2 + 6, 70, 60, 30, 20, CHORD, 2);
+
+  fill(255, 204, 153);
+  circle(width/2, height/2 + 5, 60);
+  
+  fill(255, 0, 0);
+  arc(width/2, height/2 + 24, 50, 23, 40, 20, CHORD, 2);
+  
+  fill(0);
+  arc(width/2, height/2 - 10, 60, 30, 30, 20, CHORD, 2);
+  arc(width/2 - 26, height/2 + 10, 13, 32, 30, 20, CHORD, 2);
+  arc(width/2 + 26, height/2 + 10, 13, 32, 30, 20, CHORD, 2);
+  
+  
+  fill(0);
+  rect(width/2 + 6, height/2 + 40, 15, 20);
+  rect(width/2 - 20, height/2 + 40, 15, 20);
+
+}
+
+function archer(){
+  fill(150,150,150);
+  stroke(0)
+  strokeWeight(1)
+  fill(21, 101, 192);
+  rect(width/2 - 19.5, height/2 + 25, 40, 50)
+  
+  fill(255, 204, 153);
+  circle(width/2, height/2 + 5, 60);
+
+  fill(0, 100, 0);
+  triangle(
+   width/2 - 28, height/2 - 15,   // right side
+   width/2, height/2 - 50,  // middle
+   width/2 + 28, height/2 - 15);// left side
+  
+  push()
+  translate(width/2- 15, height/2 - 22)
+  rotate(45)
+  fill(255, 0 ,0)
+  arc(0, 0, 30, 10, 30, 10, CHORD, 2);
+  pop();
+
+  fill(0, 100, 0);
+  rect(width/2 - 30, height/2 - 20, 60, 10)
+  
+  
+  }
 
 
 
@@ -84,38 +143,94 @@ class Knight{
 constructor(x,y){
 this.x = x
 this.y = y
-this.silver = color
+this.silver = color("#9fa1a0")
 }
 display(){
 this.body();
 this.hat()
+this.sward()
+this.sheild()
 }
 body(){
 fill(150,150,150);
 rect(this.x,this.y + height/34,40,50);
 fill(255, 204, 153);
-circle(this.x+20, this.y+5,60);
+circle(this.x+20, this.y+6,56);
 // fill(0)
 // point(this.x+20,this.y+5)
 }
 hat(){
   push()
-  fill(225)
+  strokeWeight(0)
+  fill(this.silver)
   translate(this.x + 20,this.y + 10)
   triangle(0,0,
-    -10,-35,
-    10,-35)
+    -10,-31,
+    10,-31)
     fill(255, 204, 153)
     push()
     strokeWeight(0)
-    circle(15,-5,19)
-    circle(15,-5,19)
+    fill(this.silver)
+    circle(13,-10,30)
+    fill(this.silver)
+    circle(0,-23,25)
+    circle(13,-18,22)
+    circle(-13,-18,22)
+    circle(0,-14,40)
+    
+
+    
+    fill(255, 204, 153)
+    circle(13,-3,22)
+    fill(this.silver)
+    circle( -13,-10,30)
+    fill(255, 204, 153)
+    circle(-13,-3,21)
+    
+
     pop()
   pop()
 }
 
+sward(){
+  push()
+  translate(this.x +55,this.y + 40 )
+  fill("#97572b")
+  rect(0,0,5,18)
+  strokeWeight(1)
+  for(let lines = 0; lines < 3; lines ++){
+    line(0,lines * 6 + 3,5, lines*6 + 3);
+  }
+  fill("#caa906")
+  strokeWeight(0)
+  rect(-4,0,13,-4)
+  fill("#c4c4c4")
+  rect(-1,-4,7,-29)
+  
+  triangle(
+    -1,-32,
+    6,-32,
+    2.5,-43
+  )
+  strokeWeight(0.5)
+  line(2.5,-4,2.5,-32)
+  strokeWeight(0)
+  fill("#caa906")
+  circle(2.5,22,6)
+  pop()
 }
 
-function archer(){
-  fill(150,150,150);
-  }
+sheild(){
+  push()
+  strokeWeight(0)
+  translate(this.x -55,this.y + 40)
+  fill("#855e42")
+  triangle(-5,-5,30,-5,15,30)
+  triangle(-5,-5,30,-5,15,-15)
+  // stroke("#caa906")
+  // strokeWeight(2)
+  // point(0,0)
+  pop()
+}
+
+}
